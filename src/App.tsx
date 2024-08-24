883067789eece9692e74b008337c84b716a1728d
koog7 <k.ognev709@mail.com>
2024-08-24 11:55:28 +0600

    Добавлена страница not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Home from "./containers/Home.tsx";
+import {Route, Routes} from "react-router-dom";
+import NotFound from "./containers/NotFound.tsx";
 
 const App = () => {
     return(
         <>
-            <Home />
+            <Routes>
+                <Route path="/" element={(
+                    <Home />
+                )}/>
+                <Route path="*" element={(
+                    <NotFound />
+                )}/>
+            </Routes>
         </>
     );
 };
